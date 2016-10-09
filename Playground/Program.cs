ee0ee77db47c46a07a80f6ca85e6364390f2cf15
sherrymall <sherrymall@live.com>
2016-10-09 16:31:43 -0700

    Use switch to control light on and off

---
+++ Playground/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Playground
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var ledPort = new OutputPort(Pins.ONBOARD_LED, false);
+            var switchPort = new InputPort(Pins.ONBOARD_BTN, false, Port.ResistorMode.Disabled);
+
+            while (true)
+            {
+                ledPort.Write(switchPort.Read());
+                Thread.Sleep(500);
+            }
+        }
+
+    }
+}
